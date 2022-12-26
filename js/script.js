dbf7dd5dbeaae91eafd46fba522b41cd9465e490
matheusleite01 <de.leite2014@gmail.com>
2022-12-26 19:17:20 -0300

    acertado o caminho do json

--- js/script.js
+++ js/script.js
@@ -39,5 +39,5 @@ const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
